f3ee0fdb26a0c6fa94925faf71f0d300a1cf6397
pamela-cl <pamelacardenas1902@gmail.com>
2022-04-22 16:44:01 -0500

    comentario

--- R/app_server.R
+++ R/app_server.R
@@ -9,7 +9,7 @@ app_server <- function(input, output, session) {
   # leer los archivos
   mod_read_files_server("archivos")
 
-  #  DGE crear tablas y validaciones de estructura
+  #  DGE Crear tablas y validaciones de estructura
   mod_dge_read_files_server("dge_val_cnsf")
 
 }
